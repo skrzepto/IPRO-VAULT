f1a98c67ca2e30ad2c078eb9e8e074e4c46fa839
wmei1 <wmei1@hawk.iit.edu>
2016-10-07 09:07:27 -0500

    fixed pi_setup.sh

--- pi_setup.sh
+++ pi_setup.sh
@@ -8,13 +8,14 @@ apt-get install build-essential python-dev python-smbus python-pip git unzip
 mkdir vault
 cd vault
 git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+wget https://github.com/adafruit/Adafruit_Python_MCP9808/archive/master.zip
+git clone https://github.com/skrzepto/IPRO-VAULT.git
 
 cd Adafruit_Python_GPIO
-python setup.py install
-
+python3 setup.py install
 cd ..
-wget https://github.com/adafruit_Python_MCP9808/archive/master.zip
+
 unzip master.zip
 rm master.zip
-
-git clone https://github.com/skrzepto/IPRO-VAULT.git
+cd Adafruit_Python_MCP9808-master
+python3 setup.py install
