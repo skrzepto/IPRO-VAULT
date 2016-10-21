5bb3351cb634f8ceaffe0b4a42ab71a6ac3aec7e
wmei <wmei1@hawk.iit.edu>
2016-10-20 20:42:20 -0500

    fixed code for testing

--- sensors/dht11.py
+++ sensors/dht11.py
@@ -3,10 +3,10 @@ from datetime import datetime, timezone
 
 class DHT11:
     def __init__(self, gpio_pin=4, sensor_ver=11):
-        self.humidity
-        self.temperature
+        self.humidity=-1
+        self.temperature=-1
         self.gpio_pin = gpio_pin
-        self.sensor_version = sensor_ver
+        self.sensor_ver= sensor_ver
         self.timestamp = datetime.now(timezone.utc).isoformat()
 
     def read_sensors(self):
@@ -17,5 +17,5 @@ class DHT11:
 
     def get_json(self):
         self.read_sensors()
-        return {"temp": self.temperature,
+        return {"temperature": self.temperature,
                 "humidity": self.humidity}
