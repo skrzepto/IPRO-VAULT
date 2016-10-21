9174e21b0d5e5d2e0258b5ec7ff1d81093e87fcf
wmei <wmei1@hawk.iit.edu>
2016-10-20 20:54:03 -0500

    removed timestamp in dht11

--- sensors/dht11.py
+++ sensors/dht11.py
@@ -7,11 +7,9 @@ class DHT11:
         self.temperature=-1
         self.gpio_pin = gpio_pin
         self.sensor_ver= sensor_ver
-        self.timestamp = datetime.now(timezone.utc).isoformat()
 
     def read_sensors(self):
         self.humidity, self.temperature = Adafruit_DHT.read_retry(self.sensor_ver, self.gpio_pin)
-        self.timestamp = datetime.now(timezone.utc).isoformat()
         if self.humidity and self.temperature:
             print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(self.temperature, self.humidity))
 
