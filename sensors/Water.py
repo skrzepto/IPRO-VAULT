301bb0da809d1529f7c5ac3323cafcc34cb616e8
tassadarforaiur <dificil1>
2016-10-20 19:48:58 -0500

    wet sensor added

---
+++ sensors/Water.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from datetime import datetime, timezone
+
+
+class Water:
+
+    def _init_(self, gpio_pin=18,pull_up_down=UP):
+        PIO.setmode(GPIO.BCM)
+        self.pull_up_down="GPIO.PUD_%s"(str(pull_up_down))
+        GPIO.setup(gpio_pin, GPIO.IN, pull_up_down)
+        self.wet
+        self.timestamp = datetime.now(timezone.utc).isoformat()
+
+    def read_sensors(self):
+        self.wet = GPIO.input(gpio_pin)
+        print('wet=%s'(self.wet))
+
+    def get_json(self):
+        self.read_sensors()
+        return {"wet": self.wet}
