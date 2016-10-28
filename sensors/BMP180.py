8cab7e755a11a4d5f8391436064c8fd5ff8e04b7
Szymon <shims506@gmail.com>
2016-10-27 20:03:44 -0500

    fixing code

--- sensors/BMP180.py
+++ sensors/BMP180.py
@@ -4,16 +4,16 @@ import Adafruit_BMP.BMP085 as BMP085
 
 class BMP180:
 
-    sensor = BMP085.BMP085()
     def _init_(self):
+        self.sensor = BMP085.BMP085()
         self.temperature
         self.pressure
-        #self.altitude
-        #self.sealevel_pressure
+        self.altitude
+        self.sealevel_pressure
 
     def read_sensors(self):
-        self.temperature = sensor.read_temperature()
-        self.pressure = sensor.read_pressure()
+        # self.temperature = self.sensor.read_temperature()
+        self.pressure = self.sensor.read_pressure()
         #self.altitude = sensor.read_altitude()
         #self.sealevel_pressure = sensor.read_sealevel_pressure()
 
